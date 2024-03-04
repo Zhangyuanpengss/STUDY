33705fda7758b79c2fb04a5293725ca3d452244c
Zhangyuanpengss <x00217230247@njit.edu.cn>
2024-03-04 21:49:23 +0800

    message

---
+++ Cprogramming/BubbleSort/BubbleSort/src/Main.java
@@ -0,0 +1,10 @@
+
+public  class Main {
+    public static void main(String[] args) throws Exception {
+        BubbleSort bubblesort=new BubbleSort();
+        int[] arr=new int[]{38,65,3,17,2};
+        bubblesort.bubbleSort(arr);
+
+  
+    }
+}
